0cb14b8addc35273cefdf5d2f9fe3e9418f19cb1
Erick Sousa <erick.v.l.s@gmail.com>
2024-10-05 14:28:10 -0300

    refactor: ContactsResponse DTO - Added Lombok annotations

--- src/main/java/com/api/aggregator/client/dto/ContactsResponse.java
+++ src/main/java/com/api/aggregator/client/dto/ContactsResponse.java
@@ -1,10 +1,14 @@
 package com.api.aggregator.client.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContactsResponse {
     private List<Contact> contacts;
 }
