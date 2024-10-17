846559400e72163062ca33ae740f87fc10fa2cae
Erick Sousa <erick.v.l.s@gmail.com>
2024-10-17 15:41:03 -0300

    refactor: Call api async - Refactor contact service for calling api async

--- src/main/java/com/api/aggregator/service/ContactService.java
+++ src/main/java/com/api/aggregator/service/ContactService.java
@@ -14,44 +14,56 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
 public class ContactService {
 
     private final KenectLabsClient kenectLabsClient;
+
     private static final String TOTAL_PAGES_HEADER = "Total-Pages";
 
     /*
     Retrieves all contacts by fetching data across multiple pages.
      */
     public List<Contact> getAllContacts(String token) {
+        var initialResponse = fetchContactsFromPage(1, token);
+
+        if (initialResponse.getBody() == null) {
+            return new ArrayList<>();
+        }
+
+        var totalPages = getTotalPages(initialResponse.getHeaders());
+        var tasks = new ArrayList<CompletableFuture<List<Contact>>>();
+
+        for (int page = 1; page <= totalPages; page++) {
+            var task = fetchContactsAsync(page, token);
+            tasks.add(task);
+        }
+
         var allContacts = new ArrayList<Contact>();
-        var page = 1;
-        int totalPages;
-
-        do {
-            var response = fetchContactsFromPage(page, token);
-            var responseBody = response.getBody();
-
-            if (responseBody != null && responseBody.getContacts() != null) {
-                // Add the source type
-                setSource(responseBody.getContacts(), SourceType.KENECT_LABS);
-                allContacts.addAll(responseBody.getContacts());
-            }
-
-            totalPages = getTotalPages(response.getHeaders());
-            page++;
-        } while (page <= totalPages);
-
+        for (var task : tasks) {
+            var result = task.join();
+            allContacts.addAll(result);
+        }
         return allContacts;
     }
+
 
     public Contact getContactById(String id, String token) {
         var contact = kenectLabsClient.getContactById(id, token);
         return Optional.ofNullable(contact.getBody())
                 .map(ContactResponse::getContact)
                 .orElseThrow(NotFoundException::new);
+    }
+
+
+    /*
+     * Sets the source type for each contact in the given list.
+     */
+    public void setSource(List<Contact> contacts, SourceType sourceType){
+        contacts.forEach(contact -> contact.setSource(sourceType));
     }
 
     /*
@@ -69,10 +81,20 @@ public class ContactService {
     }
 
     /*
-     * Sets the source type for each contact in the given list.
+     * Asynchronously fetches contacts from a specific page using the Feign client.
+     * The fetched contacts are then set with the source type {@link SourceType#KENECT_LABS}.
      */
-    public void setSource(List<Contact> contacts, SourceType sourceType){
-        contacts.forEach(contact -> contact.setSource(sourceType));
+
+    public CompletableFuture<List<Contact>> fetchContactsAsync(int page, String token) {
+        return CompletableFuture.supplyAsync(() -> {
+            var response = fetchContactsFromPage(page, token);
+            if (response.getBody() != null) {
+                var contacts = response.getBody().getContacts();
+                setSource(contacts, SourceType.KENECT_LABS);
+                return contacts;
+            }
+            return new ArrayList<>();
+        });
     }
 
 }
